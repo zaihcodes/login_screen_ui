8fb9c18d02c981585f006101748bb0fe98113422
ZI-DreamWork <zaihwork@gmail.com>
2024-05-30 20:51:59 +0100

    Created a custom button

---
+++ lib/core/utils/widgets/custom_form_button.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class CustomFormButton extends StatelessWidget {
+  const CustomFormButton({
+    super.key,
+    required this.text,
+    this.func,
+  });
+  final String text;
+  final Function()? func;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return GestureDetector(
+      onTap: func,
+      child: Container(
+        height: 54,
+        decoration: BoxDecoration(
+          color: theme.colorScheme.primary,
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(color: theme.colorScheme.onPrimary, fontSize: 18),
+          ),
+        ),
+      ),
+    );
+  }
+}
